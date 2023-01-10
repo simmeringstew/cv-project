1ae6f11c8e37d170ef0e93227af560bcd114b72a
simmeringstew <dev.95a2e@simplelogin.com>
2023-01-10 09:18:22 -0500

    Built the work display section

--- src/components/workDisplay.js
+++ src/components/workDisplay.js
@@ -2,8 +2,30 @@ const WorkDisplay = ({ data }) => {
     if (data.jobTitle === "" && data.company === "" && data.startDate === "" && data.endDate === "" && data.description === "") {
         return;
     }
+    else if (data.id === 1) {
+        return (
+            <div className="work-experience">
+                <h2 className="section-title">Experience</h2>
+                <h3 className="section-subtitle">{data.jobTitle}</h3>
+                <h4 className="section-minititle">{data.company}</h4>
+                <div className="dates">
+                    <span>From: {data.startDate}</span>
+                    <span>To: {data.endDate}</span>
+                </div>
+                <p className="description">{data.description}</p>
+            </div>
+        );
+    }
     return (
-        <h6>Hello</h6>
+        <div className="work-experience">
+            <h3 className="section-subtitle">{data.jobTitle}</h3>
+            <h4 className="section-minititle">{data.company}</h4>
+            <div className="dates">
+                <span>From: {data.startDate}</span>
+                <span>To: {data.endDate}</span>
+            </div>
+            <p className="description">{data.description}</p>
+        </div>
     );
 }
 
